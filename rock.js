69cb88dfeafde77d137bcd1b1d14589a48cc48e5
elsie <elsiechen915@gmail.com>
2022-07-30 23:40:21 -0400

    Build single round function

--- rock.js
+++ rock.js
@@ -1,3 +1,20 @@
+// Keep asking player's choice if none of rock, paper, or scissors are chosen
+let playerSelection;
+do {
+    playerSelection = prompt('Please choose between rock, paper, or scissors: ').toLowerCase();
+    console.log(`playerSelection: ${playerSelection}`);
+}
+while((playerSelection !== 'rock') &&
+      (playerSelection !== 'paper') &&
+      (playerSelection !== 'scissors'));
+
+// Get computer's choice 
+let computerSelection = getComputerChoice();
+
+// Play single round of game
+playRound(playerSelection, computerSelection);
+
+
 function getComputerChoice(){
     let array = ['Rock', 'Paper', 'Scissors'];
     let length = array.length;
@@ -7,4 +24,21 @@ function getComputerChoice(){
     return result;
 }
 
-getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+    
+    let result;
+    if (playerSelection === computerSelection.toLowerCase()){
+        result = 'You tied with computer for this round';
+    } else if ((playerSelection === 'rock' && computerSelection.toLowerCase() === 'scissors') ||
+               (playerSelection === 'paper' && computerSelection.toLowerCase() === 'rock') ||
+               (playerSelection === 'scissors' && computerSelection.toLowerCase() === 'paper')){
+        result = `You won! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        result = `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    console.log(`playerSelection: ${playerSelection}`);
+    console.log(`computerSelection: ${computerSelection}`);
+    console.log(`playRound: ${result}`);
+    return result;
+}
