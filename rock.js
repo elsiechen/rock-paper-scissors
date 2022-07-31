1bd3163a3fcf60e1b6d32e4c91eece9bd3b42af8
elsie <elsiechen915@gmail.com>
2022-07-30 22:32:59 -0400

    Get computer choice

--- rock.js
+++ rock.js
@@ -1,1 +1,10 @@
-console.log('hello, world!')
+function getComputerChoice(){
+    let array = ['Rock', 'Paper', 'Scissors'];
+    let length = array.length;
+    let randomIndex = Math.floor(Math.random()*length);
+    let result = array[randomIndex];
+    console.log(`getComputerChoice: ${result}`);
+    return result;
+}
+
+getComputerChoice();
